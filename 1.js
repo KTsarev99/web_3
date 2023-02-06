237c92c8b0898105c72f48446cea506f25448b45
KTsarev99 <kirilltcarev1999@gmail.com>
2023-02-07 02:38:36 +0300

    1

---
+++ 1.js
@@ -0,0 +1,4 @@
+const tempC=prompt("введите температуру по Цельсию")
+let tempF=((9 / 5) * tempC + 32)
+alert("температуру по Цельсию="+tempC+", а температуру по Фаренгейту="+tempF)
+console.log(tempC)
---
+++ 2.js
@@ -0,0 +1,9 @@
+
+
+function greeting()
+{
+    const name=prompt("введите имя")
+    return alert("Здравствуйте, "+name)
+}
+
+greeting()
